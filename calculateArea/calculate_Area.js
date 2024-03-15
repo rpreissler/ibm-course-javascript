c2136cca772eea2e4ce3c208f9cdcc19760aa9be
rpreissler <romano.preissler@icloud.com>
2024-03-15 08:01:48 -0400

    fixed another typo

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -3,10 +3,10 @@ let width;
 
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementById('widht').value);
+    width = parseFloat(document.getElementById('width').value);
 
     let area = length * width;
 
-    document.getElementById('result').innerText =  `The area of the rectangle is: ${area}`;
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 

be214babfee67bde91f0c3d4fcfafdd67b1a7718
rpreisler <romano.preissler@icloud.com>
2024-03-17 12:02:00 -0400

    added practice files for product survey project

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -8,6 +8,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userExperience = document.getElementById('experienceText').value;
     
   
     document.getElementById('userName').innerHTML = username;
@@ -17,6 +18,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = userExperience;
 
     document.getElementById('userInfo').style.display = 'block';
    

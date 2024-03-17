990a99218959d639d431f4138582d58b741036d4
rpreissler <romano.preissler@icloud.com>
2024-03-17 17:48:34 -0400

    add practice files to restaurant menu project

---
+++ restaurantMenu/practice.md
@@ -0,0 +1,10 @@
+# Practice task
+1. You can include prices for all the items in breakfastMenu, mainCourseMenu, and dessertMenu.
+
+2. For this you can take include the prices as follows:
+```js
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+```
+
+3. Then, access it in the similar manner and you will see the output for the menu items along with prices as well.
+![MenuCard with Price](https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-JS0101EN-SkillsNetwork/images/3_price.png "MenuCard with Price")
--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak- $30', 'Pasta- $23', 'Burger- $15', 'Salmon- $26'];
+const dessertMenu = ['Cake- $4', 'Ice Cream- $5', 'Pudding- $4', 'Fruit Salad- $8'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
